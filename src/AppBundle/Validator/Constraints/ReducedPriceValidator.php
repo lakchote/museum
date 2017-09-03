fd2ef3deae6d297f0fb021f09048583b26634e78
Lucien Akchoté <l.akchote@gmail.com>
2017-09-03 17:37:50 +0200

    - Correction contrainte personnalisée ReducedPrice

--- src/AppBundle/Validator/Constraints/ReducedPriceValidator.php
+++ src/AppBundle/Validator/Constraints/ReducedPriceValidator.php
@@ -1,15 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: BRANDON HEAT
- * Date: 14/06/2017
- * Time: 15:18
- */
 
 namespace AppBundle\Validator\Constraints;
 
-
-use AppBundle\Entity\Commande;
+use AppBundle\Entity\Billet;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 
@@ -18,15 +11,14 @@ class ReducedPriceValidator extends ConstraintValidator
 
     public function validate($value, Constraint $constraint)
     {
+        if(!$value) return;
         /**
-         * @var Commande $commande
+         * @var Billet $billet
          */
-        $commande = $this->context->getRoot()->getData();
-        $currentYear = (new \DateTime())->format('Y');
-        foreach($commande->getBillets() as $billet) {
-            if ($billet->getDateNaissance() > $currentYear && $billet->isTarifReduit()) {
-                $this->context->buildViolation($constraint->message)->addViolation();
-            }
+        $billet = $this->context->getObject();
+        $interval = (new \DateTime())->diff($billet->getDateNaissance());
+        if($interval->format('%Y') <= 12 ) {
+            $this->context->buildViolation($constraint->message)->addViolation();
         }
     }
 }
